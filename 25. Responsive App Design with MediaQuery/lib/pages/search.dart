917d8b5829f86a27c1345e46006cfb327a2817b3
Dexent Hamza <Hamza.hamdan555@gmail.com>
2020-04-20 12:44:06 +0500

    Responsive App Design Flutter

---
+++ 25. Responsive App Design with MediaQuery/lib/pages/search.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Search extends StatefulWidget {
+  @override
+  _SearchState createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  AppBar buildSearchField() {
+    return AppBar(
+        backgroundColor: Colors.white,
+        title: TextFormField(
+          decoration: InputDecoration(
+            hintText: "Search for a user...",
+            filled: true,
+            prefixIcon: Icon(
+              Icons.account_box,
+              size: 28.0,
+            ),
+            suffixIcon: IconButton(
+              icon: Icon(Icons.clear),
+              onPressed: () => print('cleared'),
+            ),
+          ),
+        ));
+  }
+
+  Container buildNoContent() {
+    final Orientation orientation = MediaQuery.of(context).orientation;
+    return Container(
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            SvgPicture.asset(
+              'assets/images/search.svg',
+              height: orientation == Orientation.portrait ? 300.0 : 200.0,
+            ),
+            Text(
+              "Find Users",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w600,
+                fontSize: 60.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
+      appBar: buildSearchField(),
+      body: buildNoContent(),
+    );
+  }
+}
+
+class UserResult extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("User Result");
+  }
+}
